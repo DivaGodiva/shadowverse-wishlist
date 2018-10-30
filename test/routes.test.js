dfdb3f4e808b8a08890b8c99815661d9db8d1d74
Simon Chae <s13chae@gmail.com>
2018-10-29 17:49:02 -0700

    travis

--- test/routes.test.js
+++ test/routes.test.js
@@ -52,91 +52,91 @@ describe('SV Wishlist', function () {
     });
   });
 
-  // describe('Login', function () {
-  //   it('Should login with correct credentials', function () {
-  //     return chai.request(app)
-  //       .post('/login')
-  //       .send({username, password})
-  //       .then(res => {
-  //         expect(res).to.have.status(200);
-  //         expect(res.body).to.be.an('object');
-  //       });
-  //   });
-  // });
+  describe('Login', function () {
+    it('Should login with correct credentials', function () {
+      return chai.request(app)
+        .post('/login')
+        .send({username, password})
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+        });
+    });
+  });
 
-  // describe('Cardsearch', function () {
-  //   it('Should get cardsearch', function () {
-  //     const agent = chai.request.agent(app);
-  //     agent.post('/login')
-  //       .send({username: 'admin', password: 'admin'})
-  //       .then(function () {
-  //         return agent.get('/cardSearch')
-  //           .then(function (res) {
-  //             expect(res).to.have.status(200);
-  //           });
-  //       });
-  //   });
-  //   it('Should post to cardsearch', function () {
-  //     const agent = chai.request.agent(app);
-  //     const newItem = {
-  //       cardId: '101211030',
-  //       priority: 'low'
-  //     };
-  //     agent.post('/login')
-  //       .send({username: 'admin', password: 'admin'})
-  //       .then(function () {
-  //         return agent.post('/cardSearch')
-  //           .send(newItem)
-  //           .then(function (res) {   
-  //             expect(res).to.have.status(201);
-  //             expect(res).to.be.json;
-  //             expect(res.body).to.be.a('object');
-  //           });
-  //       });
-  //   });
-  // });
+  describe('Cardsearch', function () {
+    it('Should get cardsearch', function () {
+      const agent = chai.request.agent(app);
+      agent.post('/login')
+        .send({username: 'admin', password: 'admin'})
+        .then(function () {
+          return agent.get('/cardSearch')
+            .then(function (res) {
+              expect(res).to.have.status(200);
+            });
+        });
+    });
+    it('Should post to cardsearch', function () {
+      const agent = chai.request.agent(app);
+      const newItem = {
+        cardId: '101211030',
+        priority: 'low'
+      };
+      agent.post('/login')
+        .send({username: 'admin', password: 'admin'})
+        .then(function () {
+          return agent.post('/cardSearch')
+            .send(newItem)
+            .then(function (res) {   
+              expect(res).to.have.status(201);
+              expect(res).to.be.json;
+              expect(res.body).to.be.a('object');
+            });
+        });
+    });
+  });
 
-  // describe('Wishlist', function () {
-  //   it('Should get wishlist', function () {
-  //     const agent = chai.request.agent(app);
-  //     agent.post('/login')
-  //       .send({username: 'admin', password: 'admin'})
-  //       .then(function () {
-  //         return agent.get('/wishList')
-  //           .then(function (res) {
-  //             expect(res).to.have.status(200);
-  //           });
-  //       });
-  //   });
-  //   it('Should edit wishlist', function () {
-  //     const agent = chai.request.agent(app);
-  //     const updateItem = {
-  //       dbId: '000000000000000000000000',
-  //       pri: 'high'
-  //     };
-  //     agent.post('/login')
-  //       .send({username: 'admin', password: 'admin'})
-  //       .then(function () {
-  //         return agent.put('/wishList')
-  //           .send(updateItem)
-  //           .then(function (res) {   
-  //             expect(res).to.have.status(200);
-  //             expect(res).to.be.json;
-  //           });
-  //       });
-  //   });
-  //   it('Should delete item in wishlist', function () {
-  //     const agent = chai.request.agent(app);
-  //     agent.post('/login')
-  //       .send({username: 'admin', password: 'admin'})
-  //       .then(function () {
-  //         return agent.delete('/wishList')
-  //           .send({dbId: '000000000000000000000001'})
-  //           .then(function (res) {
-  //             expect(res).to.have.status(204);
-  //           });
-  //       });
-  //   });
-  // });
+  describe('Wishlist', function () {
+    it('Should get wishlist', function () {
+      const agent = chai.request.agent(app);
+      agent.post('/login')
+        .send({username: 'admin', password: 'admin'})
+        .then(function () {
+          return agent.get('/wishList')
+            .then(function (res) {
+              expect(res).to.have.status(200);
+            });
+        });
+    });
+    it('Should edit wishlist', function () {
+      const agent = chai.request.agent(app);
+      const updateItem = {
+        dbId: '000000000000000000000000',
+        pri: 'high'
+      };
+      agent.post('/login')
+        .send({username: 'admin', password: 'admin'})
+        .then(function () {
+          return agent.put('/wishList')
+            .send(updateItem)
+            .then(function (res) {   
+              expect(res).to.have.status(200);
+              expect(res).to.be.json;
+            });
+        });
+    });
+    it('Should delete item in wishlist', function () {
+      const agent = chai.request.agent(app);
+      agent.post('/login')
+        .send({username: 'admin', password: 'admin'})
+        .then(function () {
+          return agent.delete('/wishList')
+            .send({dbId: '000000000000000000000001'})
+            .then(function (res) {
+              expect(res).to.have.status(204);
+            });
+        });
+    });
+  });
 
 });
