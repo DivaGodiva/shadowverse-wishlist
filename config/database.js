c69e3797ed807cd3d8a8f5e43c90005140e15988
Simon Chae <s13chae@gmail.com>
2018-11-01 13:45:13 -0700

    added mlab

--- config/database.js
+++ config/database.js
@@ -1,6 +1,16 @@
 'use strict';
 
 module.exports = {
-  'url': 'mongodb://127.0.0.1:27017/shadowverseUsers',
+  'url': getURL(),
   'testUrl': 'mongodb://127.0.0.1:27017/shadowverseUsers-test'
+};
+
+const getURL = function() {
+  const {DB_URL, DB_USER, DB_PASS} = process.env;
+  if (DB_URL && DB_USER && DB_PASS) {
+    return `mongodb://${DB_USER}:${DB_PASS}@${DB_URL}`;
+  } 
+  else {
+    return 'mongodb://127.0.0.1:27017/shadowverseUsers';
+  }
 };
