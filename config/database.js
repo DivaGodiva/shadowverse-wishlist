e9b628c4233b2cc16bb0120668c241cd093301a5
Simon Chae <s13chae@gmail.com>
2020-03-14 20:11:28 -0700

    atlas

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@
 const getURL = function() {
   const {DB_URL, DB_USER, DB_PASS} = process.env;
   if (DB_URL && DB_USER && DB_PASS) {
-    return `mongodb://${DB_USER}:${DB_PASS}@${DB_URL}`;
+    return `${DB_URL}`;
   } 
   else {
-    return 'mongodb://127.0.0.1:27017/shadowverseUsers';
+    return 'cluster0-shard-00-01-p9fnw.mongodb.net:27017';
   }
 };
 
@@ -15,3 +15,17 @@ module.exports = {
   'testUrl': 'mongodb://127.0.0.1:27017/shadowverseUsers-test'
 };
 
+// const getURL = function() {
+//   const {DB_URL, DB_USER, DB_PASS} = process.env;
+//   if (DB_URL && DB_USER && DB_PASS) {
+//     return `mongodb://${DB_USER}:${DB_PASS}@${DB_URL}`;
+//   } 
+//   else {
+//     return 'mongodb://127.0.0.1:27017/shadowverseUsers';
+//   }
+// };
+
+// module.exports = {
+//   'url': getURL(),
+//   'testUrl': 'mongodb://127.0.0.1:27017/shadowverseUsers-test'
+// };
